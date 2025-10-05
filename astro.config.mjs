d88d5f23470f9d2a406be6479b97e69afb782dab
Utkarsh Tripathi <utkarsh1999tripathi@gmail.com>
2025-10-06 01:43:48 +0530

    Routing fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://utkarshtripathi.com',
+  base: '/',
   // trailingSlash: "always",
   integrations: [mdx(), sitemap()],
 
